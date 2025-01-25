ca7b55536e5e595743a98e4c2642a9535581c951
Cyber-Uday <udaypayghon@gmail.com>
2025-01-25 13:52:40 +0530

    Thread Exteds In java

---
+++ 09-ThreadsInJava/ThreadDemo.java
@@ -0,0 +1,38 @@
+//actually the thread is a lightwieght process which help us to achive multithreading feature 
+// >> multiple threads execute the simultanously called multithreading concept..!
+
+/*
+ * Having Some methods in Thread
+ * 2 diffrent way to creating the thread in java programming language..!
+ * 1) using the extend thread class..!
+ * 2) by implement the runnable interface..!
+ * 
+ * Constructor in Thread classes..!
+ * Thread()
+ * Thread(String name)
+ * Thread(Runnable r)
+ * Thread(Runnable r,String name)
+ */
+
+class ThreadX extends Thread {
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Thread X:> "+i);
+        }
+    }
+}
+class ThreadY extends Thread {
+    public void run() {
+        for (int j = 5; j >= 0; j--) {
+            System.out.println("THREAD Y:> "+j);
+        }
+    }
+}
+class ThreadDemo {
+    public static void main(String[] args) {
+        ThreadX t1 = new ThreadX();
+        ThreadY t2 = new ThreadY();
+        t1.start();
+        t2.start();
+    }
+}
