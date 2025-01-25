3ef58f25c68b5528bea6c9c49e869f18849c367a
Cyber-Uday <udaypayghon@gmail.com>
2025-01-25 12:24:38 +0530

    Inheritance Program

---
+++ 07-Inheritance/HybridInheritance.java
@@ -0,0 +1,26 @@
+
+//combination of 2 or more inheritance called the Hybrid Inheritance..!
+import java.util.*;
+
+class Subtraction {
+    Subtraction(int num1, int num2) {
+        System.out.println("THIS IS SUBTRACTION " + (num1 - num2));
+    }
+}
+
+class Addtion extends Subtraction {
+    Addtion(int num1, int num2) {
+        super(num1, num2);
+        System.out.println("THIS IS AdDITION CLASS" + (num1 + num2));
+    }
+}
+
+public class HybridInheritance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        Addtion a1 = new Addtion(num1, num2);
+
+    }
+}
