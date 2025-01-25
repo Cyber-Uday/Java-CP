a624d76a2fbff184e12a01703a09f5b437658afe
Cyber-Uday <udaypayghon@gmail.com>
2025-01-25 12:39:21 +0530

    Inheritance Program

--- 07-Inheritance/MultipleInheritance.java
+++ 07-Inheritance/MultipleInheritance.java
@@ -1,25 +1,34 @@
 
-interface demo1
-{
+interface demo0 {
+    void pubg();
+}
+
+interface demo1 {
     void display();
 }
-interface demo2
-{
+
+interface demo2 extends demo1 {
     void Show();
 }
-public class MultipleInheritance implements demo1,demo2
+
+public class MultipleInheritance implements demo0, demo2 // or we can add more like this
 {
-    public void display()
-    {
+    public void display() {
         System.out.println("I am come from Demo 1 Interface");
     }
-    public void Show()
-    {
+
+    public void Show() {
         System.out.println("I am come from Demo 2 Interface");
     }
+
+    public void pubg() {
+        System.out.println("I AM PUBG LOVER..!");
+    }
+
     public static void main(String[] args) {
-        MultipleInheritance m=new MultipleInheritance();
+        MultipleInheritance m = new MultipleInheritance();
         m.display();
         m.Show();
-    }    
+        m.pubg();
+    }
 }
