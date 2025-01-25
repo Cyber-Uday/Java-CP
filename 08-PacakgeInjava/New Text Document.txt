f060618187482ec6748f233a79650e9d6958500d
Cyber-Uday <udaypayghon@gmail.com>
2025-01-25 14:33:14 +0530

    Packages In java

---
+++ 08-PacakgeInjava/New Text Document.txt
@@ -0,0 +1,9 @@
+package Uday;
+
+public class pubg
+{
+	public void display()
+{
+System.out.println("I AM PUBG LOVER");
+}
+}
---
+++ 08-PacakgeInjava/pubglover.java
@@ -0,0 +1,10 @@
+import Uday.*;
+
+public class pubglover
+{
+	public static void main(String[] args)
+{
+	pubg p=new pubg();
+	p.display();
+}
+}
