542d5ef6ebcb5a6a87ae57196923ae70c0f0a645
Cyber-Uday <udaypayghon@gmail.com>
2025-01-25 12:29:10 +0530

    Inheritance Program

--- 07-Inheritance/HybridInheritance.java
+++ 07-Inheritance/HybridInheritance.java
@@ -20,7 +20,6 @@ public class HybridInheritance {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        Addtion a1 = new Addtion(num1, num2);
-
-    }
+        Addtion add1=new Addtion(num1, num2);
+        }
 }
