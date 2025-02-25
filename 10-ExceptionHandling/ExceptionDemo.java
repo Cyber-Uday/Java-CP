6a4bbaa8c5e3296489de5b9c5e3c52b756790f89
Cyber-Uday <udaypayghon@gmail.com>
2025-02-25 14:17:21 +0530

    Chnages

--- 10-ExceptionHandling/ExceptionDemo.java
+++ 10-ExceptionHandling/ExceptionDemo.java
@@ -1,22 +1,20 @@
 import java.util.*;
+
 public class ExceptionDemo {
     public static void main(String[] args) {
-        //Divide By Zero Exception>> 
-        Scanner sc=new Scanner(System.in);
-        int no=sc.nextInt();
+        // Divide By Zero Exception>>
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
         System.out.println("Enter the 2 : number to divide:> ");
-        int num2=sc.nextInt();
-        try
-        {
-            if(no==0 || num2==0)
-        {
-            System.out.println("You cant Divide the number by zero");
-        }
-        else
-        {
-            System.out.println("THE DIVISION Is:"+(no/num2));
-        }
-         throw exception;
+        int num2 = sc.nextInt();
+        try {
+            if (no == 0 || num2 == 0) {
+                throw new Exception("Divide By Zero Exception ");
+            } else {
+                System.out.println("THE DIVISION Is:" + (no / num2));
+            }
+        } catch (Exception ee) {
+            System.out.println("The Excetion Occur is:> " + ee.getMessage());
         }
     }
 }
