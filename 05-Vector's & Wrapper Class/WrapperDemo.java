462b98fadd5fde386465787558ff726a8f4c0823
Cyber-Uday <udaypayghon@gmail.com>
2025-02-26 13:30:23 +0530

    Wrapper Demo

---
+++ 05-Vector's & Wrapper Class/WrapperDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        // here we are going to explore the wrapper class how we can create object's;
+
+        Float f = 10.451f;
+        Float Wi = new Float(f); // integer object Wi;
+        System.out.println("INTEGER NUMBER:> " + f);
+        System.out.println("Object Number" + Wi);
+    }
+}
