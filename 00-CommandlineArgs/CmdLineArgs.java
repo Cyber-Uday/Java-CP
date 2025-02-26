851e1ce2dfc6d9c8cf522a849a51fb9930403f60
Cyber-Uday <udaypayghon@gmail.com>
2025-02-26 11:28:13 +0530

    Command Line Args

---
+++ 00-CommandlineArgs/CmdLineArgs.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class CmdLineArgs {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        float b = Float.parseFloat(args[1]);
+
+        double add = a + b;
+        System.out.println("THE ADDITION IS:> " + add);
+    }
+}
+// after the java compile
+// while you go for the running then try to add number in last
